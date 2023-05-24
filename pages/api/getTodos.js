8850510a020270ae6f28e1f6282b708ab6bacc0c
bertfarol <efarolcamposano@gmail.com>
2023-05-24 18:27:05 +0800

    edit api getTodos

--- pages/api/getTodos.js
+++ pages/api/getTodos.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const query = `*[_type == "todo"] { _id, _createdAt, details, completed, } `;
+    const query = `*[_type == "todo"] { details } | order(_createdAt desc) `;
     const data = await sanity.fetch(query);
 
     res.status(200).json(data);
