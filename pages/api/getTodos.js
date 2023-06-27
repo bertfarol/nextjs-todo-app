f538c830f825b991d41ba4ab2b0369896d0ab14a
bert <efarolcamposano@gmail.com>
2023-06-27 12:10:18 +0800

    update getTodos desc to asc

--- pages/api/getTodos.js
+++ pages/api/getTodos.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const query = `*[_type == "todo"] { _id, details, _createdAt, completed } | order(_createdAt desc) `;
+    const query = `*[_type == "todo"] { _id, details, _createdAt, completed } | order(_createdAt asc)`;
     const data = await sanity.fetch(query);
 
     res.status(200).json(data);
